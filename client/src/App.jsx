db7eb445d29bfe548f6a8e8542aa7a7857587f66
Briankirui <84718757+Brian-Tony@users.noreply.github.com>
2024-08-17 10:31:48 +0300

    create eader components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from './pages/about';
 import Home from './pages/home';
-import Obituary from './pages/Eulogy';
+import Eulogy from './pages/Eulogy';
+import Header from './components/Header';
 
 export default function App() {
   return <BrowserRouter>
+  <Header />
   <Routes>
     <Route path='/home' element={<Home />} />
-    <Route path='/eulogy' element={<eulogy />} />
+    <Route path='/eulogy' element={<Eulogy />} />
     <Route path='/about' element={<About />} />
   </Routes>
   </BrowserRouter>;
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <h1 className='font-bold text-sm sm:text-xl flex flex-wrap '>
+            <span className=' text-slate-500'>Mama</span>
+            <span className=' text-slate-700'>Chebet</span>
+            <span></span>
+        </h1>
+    </header>
+  )
+}
